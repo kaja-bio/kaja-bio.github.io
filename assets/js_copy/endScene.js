e7bc5544a034ba8d0784ad4b5c4d5c94377dbc74
kaja-bio <130581705+kaja-bio@users.noreply.github.com>
2024-03-08 16:31:24 +0000

    Deploying to gh-pages from @ kaja-bio/kaja-bio.github.io@db99d8fa3b138af1651ae4c28694258413a8141a 🚀

--- assets/js_copy/endScene.js
+++ assets/js_copy/endScene.js
@@ -1,1 +1,1 @@
-class endScene extends Phaser.Scene{constructor(){super({key:"endScene"})}preload(){this.load.image("endbg","./img/images/end_scene.png")}create(e){this.endbg=this.add.sprite(0,0,"endbg").setOrigin(0,0)}update(e,n){}}export default endScene;
+class endScene extends Phaser.Scene{constructor(){super({key:"endScene"})}preload(){this.load.image("endbg","../img/images/end_scene.png")}create(e){this.endbg=this.add.sprite(0,0,"endbg").setOrigin(0,0)}update(e,n){}}export default endScene;
